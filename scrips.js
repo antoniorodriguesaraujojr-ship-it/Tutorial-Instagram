5473c783777cfb2879c234a6bdc6003ac713b8dd
Júnior Araújo <antoniorodriguesaraujojr-ship-it>
2026-03-23 18:54:31 -0300

    primeira alteração

---
+++ scrips.js
@@ -0,0 +1,26 @@
+
+[]
+
+
+
+
+
+
+let imagem = document.querySelector(".troca-imagem")
+
+function trocaImagem(){
+    console.log("Troca imagem")
+
+    imagem.style.opacity = 0
+}
+
+setInterval(trocaImagem, 3000)
+
+console.log(imagem)
+
+
+
+
+
+
+
